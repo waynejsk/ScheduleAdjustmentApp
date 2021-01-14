952fb5476c991fcab173dea7f2d6e7a8d8292813
wayne <juchuang123@gmail.com>
2021-01-14 12:26:47 +0900

    typo修正

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module ScheduleAdjustmentApp
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
-    config.i18.default_local = :ja
+    config.i18n.default_local = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
